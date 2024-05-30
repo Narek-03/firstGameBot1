bdc6ca78c5bf72c01f71d84ff0291daa240b0f82
Narek-03 <office5g.shopify@gmail.com>
2024-05-30 23:34:36 +0400

    fix

--- bot.js
+++ bot.js
@@ -1,21 +1,3 @@
-//const { Telegraf } = require('telegraf');
-//const path = require('path');
-//const fs = require('fs');
-
-//// Вставьте здесь ваш токен
-//const bot = new Telegraf('6653478865:AAGjiyqb_g_Vpewnje3QJkDuH2oblLGzo6c');
-
-//bot.start((ctx) => ctx.reply('Welcome! Send /game to play the game.'));
-
-//bot.command('game', (ctx) => {
-//    ctx.replyWithDocument({ source: path.join(__dirname, 'dist', 'index.html') });
-//});
-
-//bot.launch();
-
-//process.once('SIGINT', () => bot.stop('SIGINT'));
-//process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
 const { Telegraf } = require('telegraf');
 
 // Вставьте здесь ваш токен
@@ -24,7 +6,7 @@ const bot = new Telegraf('6653478865:AAGjiyqb_g_Vpewnje3QJkDuH2oblLGzo6c');
 bot.start((ctx) => ctx.reply('Welcome! Send /game to play the game.'));
 
 bot.command('game', (ctx) => {
-    ctx.reply('Click the link to play the game: https://narek-03.github.io.firstGameBot/');
+    ctx.reply('Click the link to play the game: https://Narek-03.github.io/firstGameBot1/');
 });
 
 bot.launch();
