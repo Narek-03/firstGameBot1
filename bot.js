651588e529df23e066094ddd08a9d910775ab4cd
Narek-03 <office5g.shopify@gmail.com>
2024-05-31 00:38:41 +0400

    fix

--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ const bot = new Telegraf('6653478865:AAGjiyqb_g_Vpewnje3QJkDuH2oblLGzo6c');
 bot.start((ctx) => ctx.reply('Welcome! Send /game to play the game.'));
 
 bot.command('game', (ctx) => {
-    ctx.reply('Click the link to play the game: https://Narek-03.github.io/firstGameBot1/');
+    //ctx.reply('Click the link to play the game: https://Narek-03.github.io/firstGameBot1/');
+    ctx.reply('Закрыто на ремонт');
 });
 
 bot.launch();
