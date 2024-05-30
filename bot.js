4c059c2f4a1f3cfb623d19bf2a207341681f29e7
Narek-03 <office5g.shopify@gmail.com>
2024-05-30 23:12:37 +0400

    Initial commit

--- bot.js
+++ bot.js
@@ -1,6 +1,22 @@
+//const { Telegraf } = require('telegraf');
+//const path = require('path');
+//const fs = require('fs');
+
+//// Вставьте здесь ваш токен
+//const bot = new Telegraf('6653478865:AAGjiyqb_g_Vpewnje3QJkDuH2oblLGzo6c');
+
+//bot.start((ctx) => ctx.reply('Welcome! Send /game to play the game.'));
+
+//bot.command('game', (ctx) => {
+//    ctx.replyWithDocument({ source: path.join(__dirname, 'dist', 'index.html') });
+//});
+
+//bot.launch();
+
+//process.once('SIGINT', () => bot.stop('SIGINT'));
+//process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
 const { Telegraf } = require('telegraf');
-const path = require('path');
-const fs = require('fs');
 
 // Вставьте здесь ваш токен
 const bot = new Telegraf('6653478865:AAGjiyqb_g_Vpewnje3QJkDuH2oblLGzo6c');
@@ -8,7 +24,7 @@ const bot = new Telegraf('6653478865:AAGjiyqb_g_Vpewnje3QJkDuH2oblLGzo6c');
 bot.start((ctx) => ctx.reply('Welcome! Send /game to play the game.'));
 
 bot.command('game', (ctx) => {
-    ctx.replyWithDocument({ source: path.join(__dirname, 'dist', 'index.html') });
+    ctx.reply('Click the link to play the game: https://narek-03.github.io.firstGameBot/');
 });
 
 bot.launch();
